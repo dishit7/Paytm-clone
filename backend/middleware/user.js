5cca040a5047d6bfa342804139d7538a5c1da49a
dishit7 <hanmadishit74@gmail.com>
2024-03-09 12:52:23 +0530

    Implement User Middleware
    - Add JWT authentication for user authorization
    - Integrate Zod for input validation
    
     Implement User Middleware
    - Add JWT authentication for user authorization

---
+++ backend/middleware/user.js
@@ -0,0 +1,20 @@
+const express=require("express")
+const app=express()
+const jwt=require("jsonwebtoken")
+const JWT_SECRET=require("../config")
+function userAuthorization(req,res,next){
+    try{
+    const token=req.headers.authorization
+    console.log("the token is :"+token)
+    const decodedToken=jwt.verify(token,JWT_SECRET)
+    console.log(decodedToken)
+    req.decodedToken=decodedToken
+    req.userId=decodedToken.userId
+    next()
+    console.log("hi")
+    }
+    catch(err){
+    console.log(err)
+    }
+}
+module.exports=userAuthorization
